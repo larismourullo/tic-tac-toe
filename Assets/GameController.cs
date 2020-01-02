e54beac7f9d9a47603eee707b6fcd4fb8ab7be89
Larissa Mourullo <larismourullo@gmail.com>
2020-01-01 22:03:07 -0300

    :art: Correcting project indentation

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -6,298 +6,298 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    private const string userPlayer = "X";
-    private const string computerPlayer = "O";
-    private System.Random random;
-    private string playerSide;
-    private int count;
-    public Text[] buttonList;
-    public Text gameOverText;
-    public GameObject restartButton;
-    
-    // Construtor inicializando a classe Random
-    public GameController()
+  private const string userPlayer = "X";
+  private const string computerPlayer = "O";
+  private System.Random random;
+  private string playerSide;
+  private int count;
+  public Text[] buttonList;
+  public Text gameOverText;
+  public GameObject restartButton;
+
+  // Construtor inicializando a classe Random
+  public GameController()
+  {
+    random = new System.Random();
+  }
+
+  // Função com as informações pra o início de uma partida
+  private void Awake()
+  {
+    SetGameControllerReferenceOnButtons();
+    playerSide = userPlayer;
+    count = 0;
+    restartButton.SetActive(false);
+  }
+
+  // Função que referencia os botões
+  private void SetGameControllerReferenceOnButtons()
+  {
+    for (int i = 0; i < buttonList.Length; i++)
     {
-        random = new System.Random();
+      buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+    }
+  }
+
+  // Função que verifica qual é o jogador atual
+  public string GetPlayerSide()
+  {
+    return playerSide;
+  }
+
+  // Função que finaliza o turno, ou seja, a jogada
+  public void EndTurn()
+  {
+    count++;
+
+    // Valida por meio de força bruta se o jogador possui 3 peças em sequência
+    if (((buttonList[0].text == playerSide) && (buttonList[1].text == playerSide) && (buttonList[2].text == playerSide)) ||
+         ((buttonList[3].text == playerSide) && (buttonList[4].text == playerSide) && (buttonList[5].text == playerSide)) ||
+         ((buttonList[6].text == playerSide) && (buttonList[7].text == playerSide) && (buttonList[8].text == playerSide)) ||
+         ((buttonList[0].text == playerSide) && (buttonList[3].text == playerSide) && (buttonList[6].text == playerSide)) ||
+         ((buttonList[1].text == playerSide) && (buttonList[4].text == playerSide) && (buttonList[7].text == playerSide)) ||
+         ((buttonList[2].text == playerSide) && (buttonList[5].text == playerSide) && (buttonList[8].text == playerSide)) ||
+         ((buttonList[0].text == playerSide) && (buttonList[4].text == playerSide) && (buttonList[8].text == playerSide)) ||
+         ((buttonList[2].text == playerSide) && (buttonList[4].text == playerSide) && (buttonList[6].text == playerSide)))
+    {
+      GameOver(playerSide);
+      return;
+    }
+    // Valida caso todas as peças sejam postas e nenhum usuário tenha ganhado
+    else if (count > 8)
+    {
+      GameOver("Empate");
+      return;
     }
 
-    // Função com as informações pra o início de uma partida
-    private void Awake()
+    ChangeSides();
+
+    // Verifica se é a vez do computador jogar
+    if (playerSide == computerPlayer)
     {
-        SetGameControllerReferenceOnButtons(); 
-        playerSide = userPlayer;
-        count = 0;
-        restartButton.SetActive(false);
+      ComputerThink();
+    }
+  }
+
+  // Função que efetua a troca da vez dos jogadores
+  private void ChangeSides()
+  {
+    playerSide = (playerSide == userPlayer) ? computerPlayer : userPlayer;
+  }
+
+  // Função que finaliza o jogo informando o vencedor
+  private void GameOver(string winningPlayer)
+  {
+    SetBoardInteractable(false);
+
+    if (winningPlayer == "Empate")
+    {
+      SetGameOverText("Empate!");
+    }
+    else
+    {
+      playerSide = (playerSide == userPlayer) ? "Usuário" : "Computador";
+      SetGameOverText(playerSide + " Ganhou!");
     }
 
-    // Função que referencia os botões
-    private void SetGameControllerReferenceOnButtons()
+    restartButton.SetActive(true);
+  }
+
+  // Função que permite você reiniciar o jogo novamente
+  public void RestartGame()
+  {
+    playerSide = userPlayer;
+    count = 0;
+
+    SetGameOverText("");
+    SetBoardInteractable(true);
+
+    for (int i = 0; i < buttonList.Length; i++)
     {
-        for (int i = 0; i < buttonList.Length; i++)
-        {
-            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
-        }
+      buttonList[i].text = "";
     }
 
-    // Função que verifica qual é o jogador atual
-    public string GetPlayerSide()
+    restartButton.SetActive(false);
+  }
+
+  // Função que habilita ou desabilita todos os botões
+  private void SetBoardInteractable(bool toogle)
+  {
+    for (int i = 0; i < buttonList.Length; i++)
     {
-        return playerSide;
+      buttonList[i].GetComponentInParent<Button>().interactable = toogle;
+    }
+  }
+
+  // Função que informa se a partida teve um ganhador ou foi empate
+  private void SetGameOverText(string valor)
+  {
+    gameOverText.text = valor;
+  }
+
+  // Função que prediz se na próxima jogada é possível ganhar
+  public int VerifyWin(string player)
+  {
+
+    if ((buttonList[3].text == player && CurrentPlayerCanCheck(0) && buttonList[6].text == player) ||
+        (buttonList[1].text == player && CurrentPlayerCanCheck(0) && buttonList[2].text == player) ||
+        (buttonList[4].text == player && CurrentPlayerCanCheck(0) && buttonList[8].text == player))
+    {
+      return 0;
     }
 
-    // Função que finaliza o turno, ou seja, a jogada
-    public void EndTurn()
+    if ((buttonList[0].text == player && CurrentPlayerCanCheck(1) && buttonList[2].text == player) ||
+        (buttonList[4].text == player && CurrentPlayerCanCheck(1) && buttonList[7].text == player))
     {
-        count++;
-
-        // Valida por meio de força bruta se o jogador possui 3 peças em sequência
-        if (((buttonList[0].text == playerSide) && (buttonList[1].text == playerSide) && (buttonList[2].text == playerSide)) ||
-             ((buttonList[3].text == playerSide) && (buttonList[4].text == playerSide) && (buttonList[5].text == playerSide)) ||
-             ((buttonList[6].text == playerSide) && (buttonList[7].text == playerSide) && (buttonList[8].text == playerSide)) ||
-             ((buttonList[0].text == playerSide) && (buttonList[3].text == playerSide) && (buttonList[6].text == playerSide)) ||
-             ((buttonList[1].text == playerSide) && (buttonList[4].text == playerSide) && (buttonList[7].text == playerSide)) ||
-             ((buttonList[2].text == playerSide) && (buttonList[5].text == playerSide) && (buttonList[8].text == playerSide)) ||
-             ((buttonList[0].text == playerSide) && (buttonList[4].text == playerSide) && (buttonList[8].text == playerSide)) ||
-             ((buttonList[2].text == playerSide) && (buttonList[4].text == playerSide) && (buttonList[6].text == playerSide)))
-        {
-            GameOver(playerSide);
-            return;
-        }
-        // Valida caso todas as peças sejam postas e nenhum usuário tenha ganhado
-        else if (count > 8)
-        {
-            GameOver("Empate");
-            return;
-        }
-
-        ChangeSides();
-
-        // Verifica se é a vez do computador jogar
-        if (playerSide == computerPlayer)
-        {
-            ComputerThink();
-        }
+      return 1;
     }
 
-    // Função que efetua a troca da vez dos jogadores
-    private void ChangeSides()
+    if ((buttonList[0].text == player && CurrentPlayerCanCheck(2) && buttonList[1].text == player) ||
+        (buttonList[5].text == player && CurrentPlayerCanCheck(2) && buttonList[8].text == player) ||
+        (buttonList[4].text == player && CurrentPlayerCanCheck(2) && buttonList[6].text == player))
     {
-        playerSide = (playerSide == userPlayer) ? computerPlayer : userPlayer;
+      return 2;
     }
 
-    // Função que finaliza o jogo informando o vencedor
-    private void GameOver(string winningPlayer)
+    if ((buttonList[4].text == player && CurrentPlayerCanCheck(3) && buttonList[5].text == player) ||
+        (buttonList[0].text == player && CurrentPlayerCanCheck(3) && buttonList[6].text == player))
     {
-        SetBoardInteractable(false);
-
-        if (winningPlayer == "Empate")
-        {
-            SetGameOverText("Empate!");
-        }
-        else
-        {
-            playerSide = (playerSide == userPlayer) ? "Usuário" : "Computador";
-            SetGameOverText(playerSide + " Ganhou!");
-        }
-
-        restartButton.SetActive(true);
+      return 3;
     }
 
-    // Função que permite você reiniciar o jogo novamente
-    public void RestartGame()
+    if ((buttonList[3].text == player && CurrentPlayerCanCheck(4) && buttonList[5].text == player) ||
+        (buttonList[1].text == player && CurrentPlayerCanCheck(4) && buttonList[7].text == player) ||
+        (buttonList[0].text == player && CurrentPlayerCanCheck(4) && buttonList[8].text == player) ||
+        (buttonList[2].text == player && CurrentPlayerCanCheck(4) && buttonList[6].text == player))
     {
-        playerSide = userPlayer;
-        count = 0;
-
-        SetGameOverText("");
-        SetBoardInteractable(true);
-
-        for (int i = 0; i < buttonList.Length; i++)
-        {
-            buttonList[i].text = "";
-        }
-
-        restartButton.SetActive(false);
+      return 4;
     }
 
-    // Função que habilita ou desabilita todos os botões
-    private void SetBoardInteractable(bool toogle)
+    if ((buttonList[2].text == player && CurrentPlayerCanCheck(5) && buttonList[8].text == player) ||
+        (buttonList[3].text == player && CurrentPlayerCanCheck(5) && buttonList[4].text == player))
     {
-        for (int i = 0; i < buttonList.Length; i++)
-        {
-            buttonList[i].GetComponentInParent<Button>().interactable = toogle;
-        }
+      return 5;
     }
 
-    // Função que informa se a partida teve um ganhador ou foi empate
-    private void SetGameOverText(string valor)
+    if ((buttonList[0].text == player && CurrentPlayerCanCheck(6) && buttonList[3].text == player) ||
+        (buttonList[7].text == player && CurrentPlayerCanCheck(6) && buttonList[8].text == player) ||
+        (buttonList[4].text == player && CurrentPlayerCanCheck(6) && buttonList[2].text == player))
     {
-        gameOverText.text = valor;
+      return 6;
     }
 
-    // Função que prediz se na próxima jogada é possível ganhar
-    public int VerifyWin(string player)
+    if ((buttonList[1].text == player && CurrentPlayerCanCheck(7) && buttonList[4].text == player) ||
+        (buttonList[6].text == player && CurrentPlayerCanCheck(7) && buttonList[8].text == player))
     {
-        
-        if ((buttonList[3].text == player && CurrentPlayerCanCheck(0) && buttonList[6].text == player) ||
-            (buttonList[1].text == player && CurrentPlayerCanCheck(0) && buttonList[2].text == player) ||
-            (buttonList[4].text == player && CurrentPlayerCanCheck(0) && buttonList[8].text == player))
-        {
-            return 0;
-        }
-            
-        if ((buttonList[0].text == player && CurrentPlayerCanCheck(1) && buttonList[2].text == player) ||
-            (buttonList[4].text == player && CurrentPlayerCanCheck(1) && buttonList[7].text == player))
-        {
-            return 1;
-        }
-
-        if ((buttonList[0].text == player && CurrentPlayerCanCheck(2) && buttonList[1].text == player) ||
-            (buttonList[5].text == player && CurrentPlayerCanCheck(2) && buttonList[8].text == player) ||
-            (buttonList[4].text == player && CurrentPlayerCanCheck(2) && buttonList[6].text == player))
-        {
-            return 2;
-        }
-
-        if ((buttonList[4].text == player && CurrentPlayerCanCheck(3) && buttonList[5].text == player) ||
-            (buttonList[0].text == player && CurrentPlayerCanCheck(3) && buttonList[6].text == player))
-        {
-            return 3;
-        }
-
-        if ((buttonList[3].text == player && CurrentPlayerCanCheck(4) && buttonList[5].text == player) ||
-            (buttonList[1].text == player && CurrentPlayerCanCheck(4) && buttonList[7].text == player) ||
-            (buttonList[0].text == player && CurrentPlayerCanCheck(4) && buttonList[8].text == player) ||
-            (buttonList[2].text == player && CurrentPlayerCanCheck(4) && buttonList[6].text == player))
-        {
-            return 4;
-        }
-
-        if ((buttonList[2].text == player && CurrentPlayerCanCheck(5) && buttonList[8].text == player) ||
-            (buttonList[3].text == player && CurrentPlayerCanCheck(5) && buttonList[4].text == player))
-        {
-            return 5;
-        }
-
-        if ((buttonList[0].text == player && CurrentPlayerCanCheck(6) && buttonList[3].text == player) ||
-            (buttonList[7].text == player && CurrentPlayerCanCheck(6) && buttonList[8].text == player) ||
-            (buttonList[4].text == player && CurrentPlayerCanCheck(6) && buttonList[2].text == player))
-        {
-            return 6;
-        }
-
-        if ((buttonList[1].text == player && CurrentPlayerCanCheck(7) && buttonList[4].text == player) ||
-            (buttonList[6].text == player && CurrentPlayerCanCheck(7) && buttonList[8].text == player))
-        {
-            return 7;
-        }
-
-        if ((buttonList[2].text == player && CurrentPlayerCanCheck(8) && buttonList[5].text == player) ||
-            (buttonList[6].text == player && CurrentPlayerCanCheck(8) && buttonList[7].text == player) ||
-            (buttonList[0].text == player && CurrentPlayerCanCheck(8) && buttonList[4].text == player))
-        {
-            return 8;
-        }
-
-        return 10;       
+      return 7;
     }
 
-    // Função que verifica se é a primeira jogada
-    private bool IsFirstPlay()
+    if ((buttonList[2].text == player && CurrentPlayerCanCheck(8) && buttonList[5].text == player) ||
+        (buttonList[6].text == player && CurrentPlayerCanCheck(8) && buttonList[7].text == player) ||
+        (buttonList[0].text == player && CurrentPlayerCanCheck(8) && buttonList[4].text == player))
     {
-        return count == 1;
+      return 8;
     }
 
-    // Função que faz cria a lógica do computador
-    public void ComputerThink()
+    return 10;
+  }
+
+  // Função que verifica se é a primeira jogada
+  private bool IsFirstPlay()
+  {
+    return count == 1;
+  }
+
+  // Função que faz cria a lógica do computador
+  public void ComputerThink()
+  {
+    // Verifica se o usuário jogou nos cantos
+    if (IsFirstPlay() &&
+        (buttonList[0].text == userPlayer ||
+        buttonList[2].text == userPlayer ||
+        buttonList[6].text == userPlayer ||
+        buttonList[8].text == userPlayer) &&
+        CurrentPlayerCanCheck(4))
     {
-        // Verifica se o usuário jogou nos cantos
-        if (IsFirstPlay() &&
-            (buttonList[0].text == userPlayer ||
-            buttonList[2].text == userPlayer ||
-            buttonList[6].text == userPlayer ||
-            buttonList[8].text == userPlayer) &&
-            CurrentPlayerCanCheck(4))
-        {
-            CheckButtonForCurrentPlayer(4);
-            EndTurn();
-            return;
-        }
-
-        // Verifica se o usuário jogou nas posições ao lado do centro
-        if (IsFirstPlay() &&
-            (buttonList[1].text == userPlayer ||
-            buttonList[3].text == userPlayer ||
-            buttonList[5].text == userPlayer ||
-            buttonList[7].text == userPlayer))
-        {
-            CheckRandomButton(new int[5] { 0, 2, 4, 6, 8 });
-            EndTurn();
-            return;
-        }
-
-        // Verifica se o usuário jogou no meio
-        if (IsFirstPlay() &&
-            buttonList[4].text == userPlayer)
-        {
-            CheckRandomButton(new int[4] { 0, 2, 6, 8 });
-            EndTurn();
-            return;
-        }
-
-        // Verifica se o computador pode ganhar na próxima rodada
-        int verifyComputer = VerifyWin(computerPlayer);
-
-        if (verifyComputer != 10)
-        {
-            CheckButtonForCurrentPlayer(verifyComputer);
-            EndTurn();
-            return;
-        }
-
-        // Verifica se o usuário pode ganhar na próxima rodada
-        int verifyUser = VerifyWin(userPlayer);
-
-        if (verifyUser != 10)
-        {
-            CheckButtonForCurrentPlayer(verifyUser);
-            EndTurn();
-            return;
-        }
-
-        // Caso nada seja feito anteriormente gera uma posição aleatória
-        CheckRandomButton(new int[9] { 0, 1, 2, 3, 4, 5, 6, 7, 8 });
-
-        // Finaliza o turno passando a vez para o usuário realizar sua jogada
-        EndTurn();
+      CheckButtonForCurrentPlayer(4);
+      EndTurn();
+      return;
     }
 
-    // Função que gera um local aleatório no tabuleiro
-    private void CheckRandomButton(int[] buttonIndexes)
+    // Verifica se o usuário jogou nas posições ao lado do centro
+    if (IsFirstPlay() &&
+        (buttonList[1].text == userPlayer ||
+        buttonList[3].text == userPlayer ||
+        buttonList[5].text == userPlayer ||
+        buttonList[7].text == userPlayer))
     {
-        int randomIndex = random.Next(0, buttonIndexes.Length);
-
-        while (!CurrentPlayerCanCheck(randomIndex))
-        {
-            randomIndex = random.Next(0, buttonIndexes.Length);
-        }
-
-        CheckButtonForCurrentPlayer(randomIndex);
+      CheckRandomButton(new int[5] { 0, 2, 4, 6, 8 });
+      EndTurn();
+      return;
     }
 
-    // Função que coloca a peça do computador em determinada posição
-    private void CheckButtonForCurrentPlayer(int i)
+    // Verifica se o usuário jogou no meio
+    if (IsFirstPlay() &&
+        buttonList[4].text == userPlayer)
     {
-        Color blueColor = new Color();
-        ColorUtility.TryParseHtmlString(htmlString: "#16BFBC", color: out blueColor);
-        buttonList[i].color = blueColor;
-        buttonList[i].text = computerPlayer;
-        buttonList[i].GetComponentInParent<Button>().interactable = false;
+      CheckRandomButton(new int[4] { 0, 2, 6, 8 });
+      EndTurn();
+      return;
     }
 
-    // Função que verifica se o campo já possui alguma peça
-    private bool CurrentPlayerCanCheck(int i)
+    // Verifica se o computador pode ganhar na próxima rodada
+    int verifyComputer = VerifyWin(computerPlayer);
+
+    if (verifyComputer != 10)
     {
-        return string.IsNullOrEmpty(buttonList[i].text);
+      CheckButtonForCurrentPlayer(verifyComputer);
+      EndTurn();
+      return;
     }
+
+    // Verifica se o usuário pode ganhar na próxima rodada
+    int verifyUser = VerifyWin(userPlayer);
+
+    if (verifyUser != 10)
+    {
+      CheckButtonForCurrentPlayer(verifyUser);
+      EndTurn();
+      return;
+    }
+
+    // Caso nada seja feito anteriormente gera uma posição aleatória
+    CheckRandomButton(new int[9] { 0, 1, 2, 3, 4, 5, 6, 7, 8 });
+
+    // Finaliza o turno passando a vez para o usuário realizar sua jogada
+    EndTurn();
+  }
+
+  // Função que gera um local aleatório no tabuleiro
+  private void CheckRandomButton(int[] buttonIndexes)
+  {
+    int randomIndex = random.Next(0, buttonIndexes.Length);
+
+    while (!CurrentPlayerCanCheck(randomIndex))
+    {
+      randomIndex = random.Next(0, buttonIndexes.Length);
+    }
+
+    CheckButtonForCurrentPlayer(randomIndex);
+  }
+
+  // Função que coloca a peça do computador em determinada posição
+  private void CheckButtonForCurrentPlayer(int i)
+  {
+    Color blueColor = new Color();
+    ColorUtility.TryParseHtmlString(htmlString: "#16BFBC", color: out blueColor);
+    buttonList[i].color = blueColor;
+    buttonList[i].text = computerPlayer;
+    buttonList[i].GetComponentInParent<Button>().interactable = false;
+  }
+
+  // Função que verifica se o campo já possui alguma peça
+  private bool CurrentPlayerCanCheck(int i)
+  {
+    return string.IsNullOrEmpty(buttonList[i].text);
+  }
 }
--- Assets/Scripts/GridSpace.cs
+++ Assets/Scripts/GridSpace.cs
@@ -3,31 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GridSpace : MonoBehaviour {
-    public Button button;
-    public Text buttonText;
-    private GameController gameController;
+public class GridSpace : MonoBehaviour
+{
+  public Button button;
+  public Text buttonText;
+  private GameController gameController;
 
-    // Função que inicializa a controller
-    public void SetGameControllerReference(GameController controller)
+  // Função que inicializa a controller
+  public void SetGameControllerReference(GameController controller)
+  {
+    gameController = controller;
+  }
+
+  // Função que roda ao clicar no botão e adiciona a peça do atual jogador
+  public void SetSpace()
+  {
+    buttonText.text = gameController.GetPlayerSide();
+
+    if (gameController.GetPlayerSide() == "X")
     {
-        gameController = controller;
+      Color redColor = new Color();
+      ColorUtility.TryParseHtmlString(htmlString: "#CC362E", color: out redColor);
+      buttonText.color = redColor;
     }
 
-    // Função que roda ao clicar no botão e adiciona a peça do atual jogador
-    public void SetSpace()
-    {
-        buttonText.text = gameController.GetPlayerSide();
-
-        if (gameController.GetPlayerSide() == "X")
-        {
-            Color redColor = new Color();
-            ColorUtility.TryParseHtmlString(htmlString: "#CC362E", color: out redColor);
-            buttonText.color = redColor;
-        }
-
-        button.interactable = false;
-        gameController.EndTurn();
-    }    
-
+    button.interactable = false;
+    gameController.EndTurn();
+  }
 }
